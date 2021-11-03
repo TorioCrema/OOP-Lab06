9df50d407fddab8acccd723b2f33055d05ca4e39
Simone Zama <simone.zama@studio.unibo.it>
2021-11-03 10:11:08 +0100

    Add assert tests for PositionOutOfBoundException to BaseRobotTest.java

--- src/it/unibo/oop/lab/exception1/BaseRobotTest.java
+++ src/it/unibo/oop/lab/exception1/BaseRobotTest.java
@@ -2,9 +2,12 @@ package it.unibo.oop.lab.exception1;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
+
 
 /**
  * Testing class for PositionOutOfBound.
@@ -38,7 +41,12 @@ public final class BaseRobotTest {
             assertTrue("[CHECKING MOVING RIGHT]", r1.moveRight());
         }
         // reached the right limit of the world
-        assertFalse("[CHECKING MOVING RIGHT]", r1.moveRight());
+        try {
+        	assertFalse("[CHECKING MOVING RIGHT]", r1.moveRight());
+        	fail();
+        } catch (PositionOutOfBoundException e) {
+        	assertNotNull(e);
+        }
         // checking positions x=50; y=0
         assertEquals("[MOVING RIGHT ROBOT POS X]", RobotEnvironment.WORLD_X_UPPER_LIMIT, r1.getEnvironment().getCurrPosX());
         assertEquals("[MOVING RIGHT ROBOT POS Y]", 0, r1.getEnvironment().getCurrPosY());
@@ -50,7 +58,12 @@ public final class BaseRobotTest {
             assertTrue("[CHECKING MOVING UP]", r1.moveUp());
         }
         // reached the upper limit of the world
-        assertFalse("[CHECKING MOVING UP]", r1.moveUp());
+        try {
+        	assertFalse("[CHECKING MOVING UP]", r1.moveUp());
+        	fail();
+        } catch (PositionOutOfBoundException e) {
+        	assertNotNull(e);
+        }
         // checking positions x=50; y=80
         assertEquals("[MOVING RIGHT ROBOT POS X]", RobotEnvironment.WORLD_X_UPPER_LIMIT, r1.getEnvironment().getCurrPosX());
         assertEquals("[MOVING RIGHT ROBOT POS Y]", RobotEnvironment.WORLD_Y_UPPER_LIMIT, r1.getEnvironment().getCurrPosY());
